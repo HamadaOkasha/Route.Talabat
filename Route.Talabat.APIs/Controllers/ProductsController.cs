1c821da194112892c652aa0e5534187463ffe577
HamadaOkasha <7amada.Okasha@Gmail.com>
2024-04-13 22:56:13 +0200

    get Products Endpoint

--- Route.Talabat.APIs/Controllers/ProductsController.cs
+++ Route.Talabat.APIs/Controllers/ProductsController.cs
@@ -13,5 +13,20 @@ namespace Route.Talabat.APIs.Controllers
         {
             _productsRepo = productsRepo;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productsRepo.GetAllAsync();
+
+            // JsonResult result = new JsonResult(products);
+            // result.StatusCode=200;
+            // return result;
+            
+            // OkObjectResult result = new OkObjectResult(products);
+            // return result;
+
+            return Ok(products);
+        }
     }
 }
--- Route.Talabat.Infrastructure/GenericRepository.cs
+++ Route.Talabat.Infrastructure/GenericRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Route.Talabat.Core.Entities;
+using Route.Talabat.Core.Entities.Product;
 using Route.Talabat.Core.IRepositories;
 using Route.Talabat.Infrastructure.Data;
 using System;
@@ -20,6 +21,9 @@ namespace Route.Talabat.Infrastructure
         }
         public async Task<IEnumerable<T>> GetAllAsync()
         {
+             if (typeof(T) == typeof(Product))
+                 return (IEnumerable<T>) await _dbContext.Set<Product>().Include(p=>p.Brand).Include(p => p.Category).ToListAsync();
+
             return await _dbContext.Set<T>().AsNoTracking().ToListAsync();
         }
 
