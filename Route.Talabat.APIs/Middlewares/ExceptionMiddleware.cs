d1a484ba44f29b1c8a315363f835f9a1b1461f05
HamadaOkasha <7amada.Okasha@Gmail.com>
2024-04-17 13:07:55 +0200

    factory Base - speedly Middlewares

--- Route.Talabat.APIs/Middlewares/ExceptionMiddleware.cs
+++ Route.Talabat.APIs/Middlewares/ExceptionMiddleware.cs
@@ -3,22 +3,73 @@ using System.Net;
 using System.Text.Json;
 
 namespace Route.Talabat.APIs.Middlewares
-{ 
-    
+{
+    /*By Convension Base - factory Base [implement IMiddleware] - speedly*/
+
     //1-By Convension
-    public class ExceptionMiddleware
+    //public class ExceptionMiddleware
+    //{
+    //    private readonly RequestDelegate _next;
+    //    private readonly ILogger<ExceptionMiddleware> _logger;
+    //    private readonly IWebHostEnvironment _env;
+
+    //    public ExceptionMiddleware(RequestDelegate next /*reference to next middleware */, ILogger<ExceptionMiddleware> logger, IWebHostEnvironment env)
+    //    {
+    //        _next = next;
+    //        _logger = logger;
+    //        _env = env;
+    //    }
+    //    public async Task InvokeAsync(HttpContext httpContext)
+    //    {
+    //        try
+    //        {
+
+    //            //if i want take an action with the request 
+
+    //            await _next.Invoke(httpContext);//go To next Middleware
+
+    //            //if i want take an action with the response 
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            _logger.LogError(ex.Message);          //development Env
+    //            /// log exception in (Database | Files)  //Production Env
+
+    //            //httpContext.Response.StatusCode = 500;
+    //            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+    //            httpContext.Response.ContentType = "application/json";
+
+    //            var response = _env.IsDevelopment() ?
+    //                new ApiExceptionResponse((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
+    //                :
+    //                new ApiExceptionResponse((int)HttpStatusCode.InternalServerError);
+
+
+    //            //to return to front as camelCase
+    //            var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+    //           // var json = JsonSerializer.Serialize(response);
+    //             var json = JsonSerializer.Serialize(response, options);
+
+
+    //            await httpContext.Response.WriteAsync(json);
+    //        }
+    //    }
+    //}
+
+
+    public class ExceptionMiddleware : IMiddleware
     {
-        private readonly RequestDelegate _next;
         private readonly ILogger<ExceptionMiddleware> _logger;
         private readonly IWebHostEnvironment _env;
 
-        public ExceptionMiddleware(RequestDelegate next /*reference to next middleware */, ILogger<ExceptionMiddleware> logger, IWebHostEnvironment env)
+        public ExceptionMiddleware(ILogger<ExceptionMiddleware> logger, IWebHostEnvironment env)
         {
-            _next = next;
             _logger = logger;
             _env = env;
         }
-        public async Task InvokeAsync(HttpContext httpContext)
+
+        public async Task InvokeAsync(HttpContext httpContext, RequestDelegate _next)
         {
             try
             {
@@ -42,20 +93,15 @@ namespace Route.Talabat.APIs.Middlewares
                     new ApiExceptionResponse((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
                     :
                     new ApiExceptionResponse((int)HttpStatusCode.InternalServerError);
-                
-
                 //to return to front as camelCase
                 var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
-               // var json = JsonSerializer.Serialize(response);
-                 var json = JsonSerializer.Serialize(response, options);
-
-
+                var json = JsonSerializer.Serialize(response);
+                //   var json = JsonSerializer.Serialize(response, options);
                 await httpContext.Response.WriteAsync(json);
             }
         }
+  
     }
-
-   
 }
 
--- Route.Talabat.APIs/Program.cs
+++ Route.Talabat.APIs/Program.cs
@@ -8,6 +8,8 @@ using Route.Talabat.Core.Entities.Product;
 using Route.Talabat.Core.IRepositories;
 using Route.Talabat.Infrastructure;
 using Route.Talabat.Infrastructure.Data;
+using System.Net;
+using System.Text.Json;
 
 namespace Route.Talabat.APIs
 {
@@ -74,7 +76,8 @@ namespace Route.Talabat.APIs
 
             //may be a problem when update-database and want to view or log error
             var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-        
+
+            var logger = loggerFactory.CreateLogger<Program>();
             try
             {
                 await _dbContext.Database.MigrateAsync(); //update-database
@@ -84,7 +87,6 @@ namespace Route.Talabat.APIs
             catch (Exception ex)
             {
                 //  Console.WriteLine(ex.Message);
-                var logger = loggerFactory.CreateLogger<Program>();
                 logger.LogError(ex.Message, "An error has been Occured during applaymigrations");
                // logger.LogError(ex.StackTrace.ToString());//more details
             }
@@ -97,6 +99,42 @@ namespace Route.Talabat.APIs
           
             app.UseMiddleware<ExceptionMiddleware>();
             
+            //3 speedly middleware
+            //app.Use(async (httpContext, _next) =>
+            //{
+            //    try
+            //    {
+
+            //        //if i want take an action with the request 
+
+            //        await _next.Invoke(httpContext);//go To next Middleware
+
+            //        //if i want take an action with the response 
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        logger.LogError(ex.Message);          //development Env
+            //        /// log exception in (Database | Files)  //Production Env
+
+            //        //httpContext.Response.StatusCode = 500;
+            //        httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            //        httpContext.Response.ContentType = "application/json";
+
+            //        var response = app.Environment.IsDevelopment() ?
+            //            new ApiExceptionResponse((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
+            //            :
+            //            new ApiExceptionResponse((int)HttpStatusCode.InternalServerError);
+            //        //to return to front as camelCase
+            //        var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            //        var json = JsonSerializer.Serialize(response);
+            //        //   var json = JsonSerializer.Serialize(response, options);
+            //        await httpContext.Response.WriteAsync(json);
+            //    }
+
+            //});
+
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
