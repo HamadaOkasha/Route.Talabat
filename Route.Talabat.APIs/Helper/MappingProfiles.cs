35d386a1ab7d4f723564ad40a014bfea6ef8141b
HamadaOkasha <7amada.Okasha@Gmail.com>
2024-04-17 05:48:16 +0200

    Picture Url Resolver

--- Route.Talabat.APIs/Helper/MappingProfiles.cs
+++ Route.Talabat.APIs/Helper/MappingProfiles.cs
@@ -7,11 +7,32 @@ namespace Route.Talabat.APIs.Helper
     public class MappingProfiles:Profile
     {
 
+        //// -> here no parameterless ctor defined
+        // private readonly IConfiguration _config;
+
+        // public MappingProfiles(IConfiguration config)
+        // {
+        //     _config = config;
+
+        //     CreateMap<Product, ProductToReturnDto>()
+        //         .ForMember(p => p.Brand, O => O.MapFrom(S => S.Brand.Name))
+        //         .ForMember(p => p.Category, O => O.MapFrom(S => S.Category.Name))
+        //         .ForMember(p => p.PictureUrl, O => O.MapFrom(S => $"{_config["ApiBaseUrl"]}/{S.PictureUrl}")) //error
+        //         //.reverseMap() no need here
+        //         ;
+        // }
+
+
+
         public MappingProfiles()
         {
+
+
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(p => p.Brand, O => O.MapFrom(S => S.Brand.Name))
                 .ForMember(p => p.Category, O => O.MapFrom(S => S.Category.Name))
+                //.ForMember(p => p.PictureUrl, O => O.MapFrom(S => $"{_config["ApiBaseUrl"]}/{S.PictureUrl}"))
+                .ForMember(p => p.PictureUrl, O => O.MapFrom<ProductPictureUrlResolver>())
                 //.reverseMap() no need here
                 ;
         }
---
+++ Route.Talabat.APIs/Helper/ProductPictureUrlResolver.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Route.Talabat.APIs.DTOs;
+using Route.Talabat.Core.Entities.Product;
+
+namespace Route.Talabat.APIs.Helper
+{
+    //here can resolve any thing like image calculation ....etc
+    public class ProductPictureUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _config;
+
+        public ProductPictureUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return $"{_config["ApiBaseUrl"]}/{source.PictureUrl}";
+            }
+            else
+                return string.Empty;
+        }
+    }
+}
